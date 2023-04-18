b550306b4620051e32d721e4cbf9f8fcd97f067a
EdwinDij <edwin.d899@gmail.com>
2023-04-19 01:51:53 +0200

    réglage au niveau de circleci pour le .env

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
+	
 });
 
 client.login(process.env.TOKEN);
